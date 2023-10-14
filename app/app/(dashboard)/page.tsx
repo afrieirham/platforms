7ed8046849ddfa1663645df4f59db9e3b4cf1eba
Afrie Irham <afrieirham.work@gmail.com>
2023-10-14 21:04:31 +0800

    only add margin top in small screen

--- app/app/(dashboard)/page.tsx
+++ app/app/(dashboard)/page.tsx
@@ -7,7 +7,7 @@ import CreateSiteModal from "@/components/modal/create-site";
 export default function AllSites({ params }: { params: { id: string } }) {
   return (
     <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
-      <div className="mt-8 flex flex-col space-y-6">
+      <div className="mt-8 flex flex-col space-y-6 sm:mt-0">
         <div className="flex items-center justify-between">
           <h1 className="font-cal text-3xl font-bold dark:text-white">
             All Sites
