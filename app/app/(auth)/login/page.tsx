ef57c47c998147bc4a6fb69667f4d41e74a351ff
Afrie Irham <afrieirham.work@gmail.com>
2023-10-14 21:12:01 +0800

    update login page

--- app/app/(auth)/login/page.tsx
+++ app/app/(auth)/login/page.tsx
@@ -6,25 +6,20 @@ export default function LoginPage() {
   return (
     <div className="mx-5 border border-stone-200 py-10 dark:border-stone-700 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:shadow-md">
       <Image
-        alt="Platforms Starter Kit"
+        alt="Link Cloaker"
         width={100}
         height={100}
         className="relative mx-auto h-12 w-auto dark:scale-110 dark:rounded-full dark:border dark:border-stone-400"
         src="/logo.png"
       />
       <h1 className="mt-6 text-center font-cal text-3xl dark:text-white">
-        Platforms Starter Kit
+        Link Cloaker
       </h1>
       <p className="mt-2 text-center text-sm text-stone-600 dark:text-stone-400">
-        Build multi-tenant applications with custom domains. <br />
-        <a
-          className="font-medium text-black hover:text-stone-800 dark:text-stone-300 dark:hover:text-stone-100"
-          href="https://vercel.com/blog/platforms-starter-kit"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Read the announcement.
-        </a>
+        Use custom domain to show any* website online!
+      </p>
+      <p className="text-center text-xs  text-stone-400 dark:text-stone-400">
+        *Depends if the target website allow embedding.
       </p>
 
       <div className="mx-auto mt-4 w-11/12 max-w-xs sm:w-full">
